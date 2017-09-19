fa3d2f22976d0fd438ac7679740a9c791d77505d
BaranovMykola <mapsg32@gmail.com>
2017-09-19 20:25:56 +0300

    Created WPF GUI project. Added image container

---
+++ WPF_GUI/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_GUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            var b = new BitmapImage();
+            b.BeginInit();
+            b.UriSource = new Uri(@"D:\Studying\Programming\ImageEditor\WPF_GUI\fox.jpg", UriKind.Absolute);
+            b.EndInit();
+            //image.Source = b;
+        }
+
+        private void openFile(object sender, RoutedEventArgs e)
+        {
+            var b = new BitmapImage();
+            b.BeginInit();
+            b.UriSource = new Uri(@"D:\Studying\Programming\ImageEditor\WPF_GUI\fox.jpg", UriKind.Absolute);
+            b.EndInit();
+            image.Source = b;
+        }
+    }
+}
