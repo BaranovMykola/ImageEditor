7cf18cd6064274ba83b5d4c4bd6ca7618aeadd90
BaranovMykola <mapsg32@gmail.com>
2017-09-25 10:38:32 +0300

    Added ImageStorage class, added method to lock left/right button and unlock all switches

---
+++ WPF_GUI/ImageContainer/ImageStorage.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace WPF_GUI.ImageContainer
+{
+    class ImageStorage
+    {
+        private List<Uri> imageSourses = new List<Uri>();
+        private int currentSourceIndex = 0;
+
+        public ImageStorage()
+        {
+            
+        }
+
+        public ImageStorage(string[] pathes)
+        {
+            LoadImages(pathes);
+        }
+
+        public void LoadImages(string[] pathes)
+        {
+            foreach (var path in pathes)
+            {
+                imageSourses.Add(new Uri(path, UriKind.Absolute));
+            }
+        }
+
+        public BitmapImage Current
+        {
+            get
+            {
+                var b = new BitmapImage();
+                b.BeginInit();
+                b.UriSource = imageSourses[currentSourceIndex];
+                b.EndInit();
+                return b;
+            }
+        }
+
+        public BitmapImage Next
+        {
+            get
+            {
+                ++currentSourceIndex;
+                return Current;
+            }
+        }
+
+        public BitmapImage Prev
+        {
+            get
+            {
+                --currentSourceIndex;
+                return Current;
+            }
+        }
+    }
+}
--- WPF_GUI/MainWindow.xaml.cs
+++ WPF_GUI/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Win32;
+using WPF_GUI.ImageContainer;
 
 namespace WPF_GUI
 {
@@ -20,27 +22,94 @@ namespace WPF_GUI
     /// </summary>
     public partial class MainWindow : Window
     {
+        private ImageStorage openedImage = new ImageStorage();
+
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        private void button_Click(object sender, RoutedEventArgs e)
-        {
-            var b = new BitmapImage();
-            b.BeginInit();
-            b.UriSource = new Uri(@"D:\Studying\Programming\ImageEditor\WPF_GUI\fox.jpg", UriKind.Absolute);
-            b.EndInit();
-            //image.Source = b;
-        }
-
         private void openFile(object sender, RoutedEventArgs e)
         {
-            var b = new BitmapImage();
-            b.BeginInit();
-            b.UriSource = new Uri(@"D:\Studying\Programming\ImageEditor\WPF_GUI\fox.jpg", UriKind.Absolute);
-            b.EndInit();
-            image.Source = b;
+            var openDialog = new OpenFileDialog();
+            openDialog.Multiselect = true;
+            openDialog.ShowDialog();
+            var pathes = openDialog.FileNames;
+
+            openedImage.LoadImages(pathes);
+            try
+            {
+                image.Source = openedImage.Current;
+            }
+            catch (IndexOutOfRangeException exception)
+            {
+                //Console.WriteLine(exception);
+            }
+        }
+
+        private void LeftButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                unlockLeftRightButton();
+                image.Source = openedImage.Prev;
+            }
+            catch (IndexOutOfRangeException exception)
+            {
+                Console.WriteLine(exception);
+                lockLeftButton();
+            }
+        }
+
+        private void RightButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                unlockLeftRightButton();
+                image.Source = openedImage.Next;
+            }
+            catch (IndexOutOfRangeException exception)
+            {
+                Console.WriteLine(exception);
+                lockrightButton();
+            }
+        }
+
+        private void lockLeftButton()
+        {
+            leftButton.IsEnabled = false;
+            BitmapImage leftGrayIcon = new BitmapImage();
+            leftGrayIcon.BeginInit();
+            leftGrayIcon.UriSource = new Uri(@"D:\Studying\Programming\ImageEditor\WPF_GUI\icons\left_gray.ico", UriKind.Absolute);
+            leftGrayIcon.EndInit();
+            leftIco.Source = leftGrayIcon;
+        }
+
+        private void lockrightButton()
+        {
+            rightButton.IsEnabled = false;
+            BitmapImage rightGrayIcon = new BitmapImage();
+            rightGrayIcon.BeginInit();
+            rightGrayIcon.UriSource = new Uri(@"D:\Studying\Programming\ImageEditor\WPF_GUI\icons\right_gray.ico", UriKind.Absolute);
+            rightGrayIcon.EndInit();
+            rightIco.Source = rightGrayIcon;
+        }
+
+        private void unlockLeftRightButton()
+        {
+            rightButton.IsEnabled = true;
+            BitmapImage rightGrayIcon = new BitmapImage();
+            rightGrayIcon.BeginInit();
+            rightGrayIcon.UriSource = new Uri(@"D:\Studying\Programming\ImageEditor\WPF_GUI\icons\right.ico", UriKind.Absolute);
+            rightGrayIcon.EndInit();
+            rightIco.Source = rightGrayIcon;
+
+            leftButton.IsEnabled = true;
+            BitmapImage leftGrayIcon = new BitmapImage();
+            leftGrayIcon.BeginInit();
+            leftGrayIcon.UriSource = new Uri(@"D:\Studying\Programming\ImageEditor\WPF_GUI\icons\left.ico", UriKind.Absolute);
+            leftGrayIcon.EndInit();
+            leftIco.Source = leftGrayIcon;
         }
     }
 }
