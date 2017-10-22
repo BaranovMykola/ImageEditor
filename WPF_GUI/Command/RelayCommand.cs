c49910aa7d36fe08e77e595765ba7d30899df90d
BaranovMykola <mapsg32@gmail.com>
2017-10-22 22:04:30 +0300

    Added ViewModel clsss and RelayCommand class

---
+++ WPF_GUI/Command/RelayCommand.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Input;
+
+namespace WPF_GUI.Command
+{
+    /// <summary>
+    /// A command whose sole purpose is to 
+    /// relay its functionality to other
+    /// objects by invoking delegates. The
+    /// default return value for the CanExecute
+    /// method is 'true'.
+    /// </summary>
+    public class RelayCommand : ICommand
+    {
+        #region Fields
+
+        private readonly Action<object> execute;
+
+        private readonly Action<object> executeParam;
+
+        private readonly Predicate<object> canExecute;
+
+        #endregion // Fields
+
+        #region Constructors
+
+        /// <summary>
+        /// Creates a new command that can always execute.
+        /// </summary>
+        /// <param name="execute">The execution logic.</param>
+        public RelayCommand(Action<object> execute)
+            : this(execute, null)
+        {
+        }
+
+        /// <summary>
+        /// Creates a new command.
+        /// </summary>
+        /// <param name="execute">The execution logic.</param>
+        /// <param name="canExecute">The execution status logic.</param>
+        public RelayCommand(Action<object> execute, Predicate<object> canExecute)
+        {
+            if (execute == null)
+            {
+                throw new ArgumentNullException(nameof(execute));
+            }
+
+            this.execute = execute;
+            this.canExecute = canExecute;
+        }
+
+        public event EventHandler CanExecuteChanged;
+        //{
+        //    add { CommandManager.RequerySuggested += value; }
+        //    remove { CommandManager.RequerySuggested -= value; }
+        //}
+
+        public void RaiseCanExecuteChanged()
+        {
+            CanExecuteChanged?.Invoke(this, new EventArgs());
+        }
+
+        #endregion // Constructors
+
+        #region ICommand Members
+
+        [DebuggerStepThrough]
+        public bool CanExecute(object parameters) => canExecute?.Invoke(parameters) ?? true;
+
+        public void Execute(object parameters)
+        {
+            execute(parameters);
+        }
+
+        #endregion // ICommand Members
+    }
+}
