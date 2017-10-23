169d3d52410b090b1fafa75ce67bc1d68c6e7633
BaranovMykola <mapsg32@gmail.com>
2017-10-23 10:37:56 +0300

    Binded preview icons to listview

--- WPF_GUI/ViewModel.cs
+++ WPF_GUI/ViewModel.cs
@@ -1,14 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Media;
+using System.Windows.Media.Imaging;
 using CoreWrapper;
 using Microsoft.Win32;
 using WPF_GUI.Command;
+using WPF_GUI.Const;
 using WPF_GUI.ImageContainer;
 
 namespace WPF_GUI
@@ -19,7 +24,22 @@ namespace WPF_GUI
 
         public ImageStorageModel OpenedImage { get; set; }
 
+        public ObservableCollection<Image> ImagesPreview
+        {
+            get
+            {
+                return _imagesPreview;
+                OnPropertyChanged("ImagesPreview");
+            }
+            set
+            {
+                _imagesPreview = value;
+                OnPropertyChanged("ImagesPreview");
+            }
+        }
+
         private ImageProc editor = new ImageProc();
+        private ObservableCollection<Image> _imagesPreview = new ObservableCollection<Image>();
 
         public ViewModel()
         {
@@ -48,6 +68,35 @@ namespace WPF_GUI
             var pathes = openDialog.FileNames;
 
             OpenedImage.LoadImages(pathes);
+            LoadPreviews(OpenedImage.GetAllPathes());
+        }
+
+        private void LoadPreviews(string[] pathes)
+        {
+            foreach (var path in pathes)
+            {
+                var original = new BitmapImage();
+                original.BeginInit();
+                original.UriSource = new Uri(path);
+                original.EndInit();
+
+                double ratio = Constants.PreviewWidth / original.Width;
+                var reducedPreview = new BitmapImage();
+                reducedPreview.BeginInit();
+                reducedPreview.UriSource = new Uri(path);
+                reducedPreview.DecodePixelWidth = Constants.PreviewWidth;
+                reducedPreview.DecodePixelHeight = (int)(original.Height * ratio);
+                reducedPreview.EndInit();
+
+                var im = new Image
+                {
+                    Source = reducedPreview,
+                    Height = Constants.PreviewHeight,
+                    HorizontalAlignment = HorizontalAlignment.Center
+                };
+                //preview.Items.Add(im);
+                ImagesPreview.Add(im);
+            }
         }
     }
 }
