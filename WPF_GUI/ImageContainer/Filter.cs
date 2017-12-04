8e579a94a0ac34751afabc0fd66ec3d986991f83
BaranovMykola <mapsg32@gmail.com>
2017-12-04 21:48:11 +0200

    Added Filter class

---
+++ WPF_GUI/ImageContainer/Filter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_GUI.ImageContainer
+{
+    public class Filter
+    {
+        public List<List<double>> Matrix { get; set; }
+
+        public Filter()
+        {
+            Matrix = new List<List<double>>();
+        }
+
+        public Filter(int rows, int cols)
+        {
+            for (int i = 0; i < rows; i++)
+            {
+                Matrix.Add( new List<double>(cols));
+            }
+        }
+
+        public Filter(List<List<double>> matrix)
+        {
+            Matrix = matrix;
+        }
+    }
+}
