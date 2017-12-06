94572420cf5273ca670af58dfcddf1960780bb07
BaranovMykola <mapsg32@gmail.com>
2017-12-06 13:25:11 +0200

    Refactored BoolToThicknessConverter

--- WPF_GUI/Const/Constants.cs
+++ WPF_GUI/Const/Constants.cs
@@ -7,5 +7,9 @@
         public static int PreviewHeight => 100;
 
         public static int MaxContrast => 3;
+
+        public static int DefaultMatrixItemThickness => 1;
+
+        public static int AnchorMatrixItemThickness => 3;
     }
 }
